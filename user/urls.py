f1d0504232eeeaa8fa8d807fe4fda37a7d7deaa1
anupamthe99 <puspaanupam@gmail.com>
2023-03-09 20:30:41 +0545

    used dj inbuilt auth sys

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from .views import register ,home
 
 urlpatterns = [
     path('home/',home,name="home"),
-    path('register',register,name="register-user")
+    path('register',register,name="register-user"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
